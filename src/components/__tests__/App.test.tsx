f321d9b3d85ca4da272691964769c2868ce945e4
Tomy Reyes <tomy@peacegeeks.org>
2019-09-03 09:03:05 -0700

    Issue #3 move component test to new dir

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -1,7 +1,7 @@
 // tslint:disable:no-expression-statement
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import { App } from '../App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');

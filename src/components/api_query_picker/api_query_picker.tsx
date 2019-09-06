61cff051c7d3d30c2b88f3caec05bd18e8c2ea33
Tomy Reyes <tomy@peacegeeks.org>
2019-09-06 10:14:10 -0700

    Issue #3 create hook for validation

--- src/components/api_query_picker/api_query_picker.tsx
+++ src/components/api_query_picker/api_query_picker.tsx
@@ -1,5 +1,5 @@
 // tslint:disable:no-expression-statement
-import React, { useState, Dispatch, SetStateAction } from 'react';
+import React, { useState, Dispatch, SetStateAction, useEffect } from 'react';
 import { Dropdown } from '../dropdown/dropdown';
 import { topicsForQA } from '../dropdown/dropdown_data/topics';
 import { manualLocationsForQA } from '../dropdown/dropdown_data/manual_locations';
@@ -23,6 +23,7 @@ type Props = APIQueryPickerProps & APIQueryPickerActions;
 export const APIQueryPicker = (props: Props): JSX.Element => {
     const [selectedTopic, setTopic]: [SelectedOption, SetOption] = useState('');
     const [selectedManualLocation, setManualLocation]: [SelectedOption, SetOption] = useState('');
+    console.log(selectedManualLocation);
     const onSetTopic = (event: React.ChangeEvent<HTMLSelectElement>): void => setTopic(event.target.value);
     const onSetManualLocation = (event: React.ChangeEvent<HTMLSelectElement>): void => setManualLocation(event.target.value);
     const clearSelectedOptions = (): void => {
@@ -57,16 +58,28 @@ export interface SendButtonProps {
     readonly setServices: SetServices;
 }
 
-const SendButton = (props: SendButtonProps): JSX.Element => (
-    <button onClick={(): Promise<void> => updateServices(props.selectedTopic, props.selectedManualLocation, props.setServices)}>
-        Send
-    </button>
- );
+export type IsDisabled = boolean;
+
+export type SetDisabled = Dispatch<SetStateAction<boolean>>;
+
+const SendButton = (props: SendButtonProps): JSX.Element => {
+    const [isDisabled, setDisabled]: [IsDisabled, SetDisabled] = useState<IsDisabled>(true);
+    useEffect(() => {
+        if (props.selectedTopic && props.selectedManualLocation) {
+          setDisabled(false);
+        }
+        setDisabled(true);
+      }, [props.selectedTopic, props.selectedManualLocation]);
+    return (
+        <button
+            disabled={isDisabled}
+            onClick={(): Promise<void> => updateServices(props.selectedTopic, props.selectedManualLocation, props.setServices)}>
+            Send
+        </button>
+    );
+}
 
 const updateServices = async (topic: SelectedOption, manualLocation: SelectedOption, setServices: SetServices): Promise<void> => {
-    if (!topic || !manualLocation) {
-        return alert('you must select a topic and location');
-    }
     setServices({type: 'Services:Loading'});
     const servicesAtLocationJSON = await searchServices(topic, manualLocation);
     setServices(servicesAtLocationJSON);

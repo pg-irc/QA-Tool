aebe29ad52c2d3874f36c8b254395359ba7c80e6
Tomy Reyes <tomy@peacegeeks.org>
2019-10-22 09:35:20 -0700

    Issue #18 add more location fixtures for QA

--- src/fixtures/dropdown_data/locations.ts
+++ src/fixtures/dropdown_data/locations.ts
@@ -2,18 +2,45 @@ import { DropdownItemCollection } from './types';
 
 export const locationsForQA: DropdownItemCollection = [
     {
-        name: 'Vancouver', lat_long: '-123.1207, +49.2827',
-    },
-    {
-        name: 'Surrey', lat_long: '-122.8490,+49.1913',
-    },
-    {
-        name: 'New Westminister', lat_long: '-122.9110, +49.2057',
+        name: 'Burnaby', lat_long: '-122.9805, +49.2488',
     },
     {
         name: 'Coquitlam', lat_long: '-122.7932, +49.2838',
     },
     {
-        name: 'Burnaby', lat_long: '-122.9805, +49.2488',
+        name: 'Delta', lat_long: '-123.0265,+49.0952',
+    },
+    {
+        name: 'Kelowna', lat_long: '-119.4960,+49.8880',
+    },
+    {
+        name: 'Langley', lat_long: '-122.6604,+49.1042',
+    },
+    {
+        name: 'New Westminister', lat_long: '-122.9110, +49.2057',
+    },
+    {
+        name: 'North Vancouver', lat_long: '-123.0724,+49.3200',
+    },
+    {
+        name: 'Port Moody', lat_long: '-122.8678,+49.2849',
+    },
+    {
+        name: 'Prince George', lat_long: '-122.7497,+53.9171',
+    },
+    {
+        name: 'Richmond', lat_long: '-123.1336,+49.1666',
+    },
+    {
+        name: 'Surrey', lat_long: '-122.8490,+49.1913',
+    },
+    {
+        name: 'Vancouver', lat_long: '-123.1207, +49.2827',
+    },
+    {
+        name: 'Victoria', lat_long: '-123.3656,+48.4284',
+    },
+    {
+        name: 'West Vancouver', lat_long: '-123.1602,+49.3286',
     },
 ];

d236f4461034087d18a1dc0a430aff85e10443c3
Tomy Reyes <tomy@peacegeeks.org>
2019-10-23 14:27:25 -0700

    Issue #18 improve error handling using exception

--- src/api/exceptions.ts
+++ src/api/exceptions.ts
@@ -1,5 +1,7 @@
 // tslint:disable: no-class no-this no-expression-statement
 import { InvalidServices } from '../components/services/types';
+import { InvalidLocations, InvalidTopics } from '../application/types';
+import { InvalidAlgorithms } from './types';
 
 export class ValidationException extends Error {
     constructor(m: string) {
@@ -8,5 +10,14 @@ export class ValidationException extends Error {
     }
     buildErrorServiceType(): InvalidServices {
         return { type: 'Services:Error', errorMessage: this.message };
+    }
+    buildErrorLocationsType(): InvalidLocations {
+        return { type: 'Locations:Error', errorMessage: this.message };
+    }
+    buildErrorTopicsType(): InvalidTopics {
+        return { type: 'Topics:Error', errorMessage: this.message };
+    }
+    buildErrorAlgorithmsType(): InvalidAlgorithms {
+        return { type: 'Algorithms:Error', errorMessage: this.message };
     }
 }

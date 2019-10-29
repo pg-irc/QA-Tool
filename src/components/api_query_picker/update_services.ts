9a97b18b659a96aaca3d4722af31e52c5465930a
Tomy Reyes <tomy@peacegeeks.org>
2019-10-29 13:48:50 -0700

    Issue #18 fix update services error handling

--- src/components/api_query_picker/update_services.ts
+++ src/components/api_query_picker/update_services.ts
@@ -3,7 +3,8 @@ import { requestServices, validateServicesResponse } from '../../api/services';
 import { ApiQueryPickerProps } from './api_query_picker';
 import { ValidAlgorithms, SetAlgorithmId, Locations, Location, Topics, Algorithm } from '../../application/types';
 import { LocationId } from './types';
-import { buildServicesLoadingType, buildSuccessLocationsCollectionType, buildEmptyLocationsCollectionType, buildEmptyTopicsCollectionType, buildSuccessTopicsCollectionsType } from '../../application/build_types';
+import { buildServicesLoadingType, buildSuccessLocationsCollectionType, buildEmptyLocationsCollectionType,
+        buildEmptyTopicsCollectionType, buildSuccessTopicsCollectionsType } from '../../application/build_types';
 import * as constants from '../../application/constants';
 import { LocationsCollection, TopicsCollection } from '../dropdown/types';
 
@@ -21,15 +22,11 @@ const getRandomAlgorithmUrl = (algorithms: ValidAlgorithms, setAlgorithm: SetAlg
 };
 
 const getServices = async (props: ApiQueryPickerProps, randomAlgorithmUrl: string): Promise<void> => {
-    try {
         const selectedLocationLongLat = buildLongLatFromId(props.location, props.locations);
         const servicesResponse = await requestServices(props.topic, selectedLocationLongLat, randomAlgorithmUrl);
         props.setServices(buildServicesLoadingType());
-        const successServices = validateServicesResponse(servicesResponse);
-        props.setServices(successServices);
-    } catch (error) {
-        props.setServices(error.buildErrorServiceType());
-    }
+        const services = validateServicesResponse(servicesResponse);
+        props.setServices(services);
 };
 
 const buildLongLatFromId = (selectedLocation: LocationId, locations: Locations): Location => {

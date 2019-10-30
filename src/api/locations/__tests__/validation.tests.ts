70a1b6cd98349b5fa1819babe31683bc57964a1c
Tomy Reyes <tomy@peacegeeks.org>
2019-10-30 11:04:26 -0700

    Issue #18 tests for schema validation and remove unneeded test

---
+++ src/api/locations/__tests__/validation.tests.ts
@@ -0,0 +1,28 @@
+// tslint:disable:no-expression-statement
+import { aNumber, aString } from '../../../pathways-frontend/src/helpers/random_test_values';
+import { validateLocationsArray } from '../validate';
+
+describe('Locations response validation', () => {
+    describe('with valid data', () => {
+        it('passes schema validation', () => {
+            const validator = validateLocationsArray([{
+                id: aNumber(),
+                name: aString(),
+                latitude: aNumber(),
+                longitude: aNumber(),
+            }]);
+            expect(validator.isValid).toBe(true);
+        });
+    });
+    describe('with invalid data', () => {
+        it('fails schema validation', () => {
+            const validator = validateLocationsArray([{
+                id: aNumber(),
+                name: aString(),
+                latitude: aString(),
+                longitude: aString(),
+            }]);
+            expect(validator.isValid).toBe(false);
+        });
+    });
+});

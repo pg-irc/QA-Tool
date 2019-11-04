732b2567f24a890cca279109451748f63fb77bec
Tomy Reyes <tomy@peacegeeks.org>
2019-11-04 15:12:44 -0800

    Issue #18 DRY up validation code

--- src/api/algorithms/__tests__/validation.tests.ts
+++ src/api/algorithms/__tests__/validation.tests.ts
@@ -1,25 +1,32 @@
 // tslint:disable:no-expression-statement
 import { anInteger, aString } from '../../../pathways-frontend/src/helpers/random_test_values';
-import { validateAlgorithmsArray } from '../validate';
+import { validateIncomingData } from '../../validation';
+import { algorithmsArray } from '../schema';
 
 describe('Algorithms response validation', () => {
     describe('with valid data', () => {
         it('passes schema validation', () => {
-            const validator = validateAlgorithmsArray([{
-                id: anInteger(),
-                url: aString(),
-                name: aString(),
-            }]);
+            const validator = validateIncomingData(
+                algorithmsArray,
+                [{
+                    id: anInteger(),
+                    url: aString(),
+                    name: aString(),
+                }],
+            );
             expect(validator.isValid).toBe(true);
         });
     });
     describe('with invalid data', () => {
         it('fails schema validation', () => {
-            const validator = validateAlgorithmsArray([{
-                // id: anInteger(),
-                url: aString(),
-                name: aString(),
-            }]);
+            const validator = validateIncomingData(
+                algorithmsArray,
+                [{
+                    // id: anInteger(),
+                    url: aString(),
+                    name: aString(),
+                }],
+            );
             expect(validator.isValid).toBe(false);
         });
     });
---
+++ src/api/validation.ts
@@ -0,0 +1,10 @@
+import { UnvalidatedData, ValidationResult, IncomingDataSchema } from './types';
+
+// tslint:disable-next-line:no-var-requires
+const Ajv = require('ajv');
+
+export const validateIncomingData = (schema: IncomingDataSchema, data: UnvalidatedData): ValidationResult => {
+    const ajv = new Ajv();
+    const isValid = ajv.validate(schema, data) as boolean;
+    return isValid ? { isValid } : { isValid, errors: ajv.errorsText(ajv.errors) };
+};

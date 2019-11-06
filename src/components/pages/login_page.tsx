6c516e53dcca28a6ca79fab2e4c9109920d706cd
Tomy Reyes <tomy@peacegeeks.org>
2019-11-06 14:13:39 -0800

    Issue #26 create form using react-hook-form

--- src/components/pages/login_page.tsx
+++ src/components/pages/login_page.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import useForm from 'react-hook-form';
 
-export const Login = (): JSX.Element => (
-    <div>
-        Login
-    </div>
-);
+export interface FormData {
+    readonly username: string;
+    readonly password: string;
+}
+
+export const Login = (): JSX.Element => {
+// tslint:disable-next-line: typedef
+  const { register, handleSubmit, errors } = useForm<FormData>();
+  const onSubmit = (data: FormData): void => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Login</h1>
+        <label>Username</label>
+        <input name='username' ref={register({ required: true })} />
+        {errors.username && 'Username is required'}
+        <label>Password</label>
+        <input name='password' type='password' ref={register({ required: true})} />
+        {errors.password && 'Password is required'}
+      <input type='submit' />
+    </form>
+  );
+};

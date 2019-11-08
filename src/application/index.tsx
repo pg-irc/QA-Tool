45b7e84690cdedc1a94e7f17f199c186ea41dfde
Tomy Reyes <tomy@peacegeeks.org>
2019-11-07 16:51:55 -0800

    Issue #26 add the different state types for user login

--- src/application/index.tsx
+++ src/application/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ServiceRelevancyScore } from '../components/pages/service_relevancy_score';
 import { Login } from '../components/pages/login_page';
+import { User, SetUser } from './types';
+import { buildEmptyUserType } from './build_types';
+import { RouteChildrenProps } from 'react-router';
 
-export const Application = (): JSX.Element => (
-    <Router>
-        <Switch>
-            <Route exact path={'/'} component={ServiceRelevancyScore}/>
-            <Route exact path={'/login'} component={Login} />
-        </Switch>
-    </Router>
-);
+export const Application = (): JSX.Element => {
+    const [user, setUser]: [User, SetUser] = useState<User>(buildEmptyUserType);
+    return (
+        <Router>
+            <Switch>
+                <Route exact path={'/'} component={ServiceRelevancyScore}/>
+                <Route exact path={'/login'} render={(props: RouteChildrenProps): JSX.Element =>
+                    <Login {...props} user={user} setUser={setUser}/>} />
+            </Switch>
+        </Router>
+        );
+};
--- src/components/pages/login_page.tsx
+++ src/components/pages/login_page.tsx
@@ -1,26 +1,55 @@
+// tslint:disable: no-expression-statement
 import React from 'react';
 import useForm from 'react-hook-form';
 import { requestLogin } from '../../api/login';
+import { RouteChildrenProps } from 'react-router';
+import { SetUser, User } from '../../application/types';
+import * as constants from '../../application/constants';
 
 export interface LoginData {
     readonly username: string;
     readonly password: string;
 }
 
-export const Login = (): JSX.Element => {
+export interface LoginProps {
+  readonly user: User;
+  readonly setUser: SetUser;
+}
+
+type Props = LoginProps & RouteChildrenProps;
+
+export const Login = (props: Props): JSX.Element => {
 // tslint:disable-next-line: typedef
   const { register, handleSubmit, errors } = useForm<LoginData>();
+  const getUserFromApi = async (loginData: LoginData): Promise<void> => {
+    const userState = await requestLogin(loginData);
+    props.setUser(userState);
+  };
 
   return (
-    <form onSubmit={handleSubmit(requestLogin)}>
-        <h1>Login</h1>
-        <label>Username</label>
-        <input name='username' ref={register({ required: true })} />
-        {errors.username && 'Username is required'}
-        <label>Password</label>
-        <input name='password' type='password' ref={register({ required: true})} />
-        {errors.password && 'Password is required'}
-      <input type='submit' />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit(getUserFromApi)}>
+          <h1>Login</h1>
+          <label>Username</label>
+          <input name='username' ref={register({ required: true })} />
+          {errors.username && 'Username is required'}
+          <label>Password</label>
+          <input name='password' type='password' ref={register({ required: true})} />
+          {errors.password && 'Password is required'}
+        <input type='submit' />
+      </form>
+      {renderUserStatusMessage(props.user)}
+    </div>
   );
+};
+
+const renderUserStatusMessage = (user: User): JSX.Element | void => {
+  switch (user.type) {
+    case constants.USER_VALID:
+      return <h2>Login Success!</h2>;
+    case constants.USER_INVALID:
+    return <h2>Username/Password invalid. Please try again. </h2>;
+    default:
+      return <div></div>;
+  }
 };

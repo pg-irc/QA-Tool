5d5b9dd45e93a170cf897e04976be2d64caae121
Tomy Reyes <tomy@peacegeeks.org>
2019-11-19 12:01:20 -0800

    Issue #26 persist user login by checking if token exists

--- src/application/index.tsx
+++ src/application/index.tsx
@@ -1,12 +1,16 @@
 // tslint:disable: no-expression-statement
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { User, SetUser } from './types';
 import { buildEmptyUserType } from './build_types';
 import { PageSwitcher } from '../components/page_switcher/page_switcher';
+import { buildInitialUserType } from './build_initial_states';
 
 export const Application = (): JSX.Element => {
     const [user, setUser]: [User, SetUser] = useState<User>(buildEmptyUserType);
+    useEffect(() => {
+        buildInitialUserType(setUser);
+      }, []);
     return (
         <Router>
             <PageSwitcher user={user} setUser={setUser}/>
--- src/components/page_switcher/page_switcher.tsx
+++ src/components/page_switcher/page_switcher.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { RouteChildrenProps } from 'react-router';
 import { UserProps } from '../../application/types';
 import { ServiceRelevancyScore } from '../pages/service_relevancy_score';
 import { Login } from '../pages/login';
 import { PrivateRoute } from './private_route';
+import * as constants from '../../application/constants';
 
 export const PageSwitcher = (props: UserProps): JSX.Element => {
-    const renderLogin = (renderProps: RouteChildrenProps): JSX.Element => (
-        <Login {...renderProps} user={props.user} setUser={props.setUser} />
-    );
+    const renderLoginOrRedirect = (renderProps: RouteChildrenProps): JSX.Element => {
+        if (props.user.type !== constants.USER_VALID) {
+            return <Login {...renderProps} user={props.user} setUser={props.setUser} />;
+        }
+        return <Redirect to='/'/>;
+    };
     return (
         <Switch>
-        <Route exact path={'/login'} render={renderLogin} />
+        <Route exact path={'/login'} render={renderLoginOrRedirect} />;
         <PrivateRoute user={props.user} setUser={props.setUser} Component={ServiceRelevancyScore}/>
     </Switch>
     );
